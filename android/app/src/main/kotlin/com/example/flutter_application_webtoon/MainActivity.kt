7bfdbe1d1d4babbef223f160d487fe9e66153659
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-03-21 15:30:57 +0900

    초기커밋

---
+++ android/app/src/main/kotlin/com/example/flutter_application_webtoon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_webtoon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          'WebToons',
+          style: TextStyle(
+              color: Colors.indigo.shade900, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
